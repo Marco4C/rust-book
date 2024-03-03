394165cc08f215f09497a647b7a92841cc2907f8
Marco4c <marco4coutinho@gmail.com>
2024-03-03 18:26:25 -0300

    Shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+            //x =  5 + 1 / 6
+    let x = x + 1;
+            //x = 6 * 2 / 12
+    let x = x * 2;
+
+    println!("O valor de x é: {}", x);
 }
